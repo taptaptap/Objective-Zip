cec4ccce5477acce93e944a5177dc3cd67526f03
Julián Romero <julian@latenitesoft.com>
2015-12-30 13:50:46 +0100

    Framework w/ no encryption.
    Added a framework target with encryption disabled.

---
+++ ObjectiveZip/ObjectiveZip.h
@@ -0,0 +1,28 @@
+//
+//  ObjectiveZip.h
+//  ObjectiveZip
+//
+//  Created by Julián Romero on 28/12/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ObjectiveZip.
+FOUNDATION_EXPORT double ObjectiveZipVersionNumber;
+
+//! Project version string for ObjectiveZip.
+FOUNDATION_EXPORT const unsigned char ObjectiveZipVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ObjectiveZip/PublicHeader.h>
+
+#import "OZZipFile.h"
+#import "OZZipFile+Standard.h"
+#import "OZZipFileMode.h"
+#import "OZZipCompressionLevel.h"
+#import "OZZipException.h"
+#import "OZZipWriteStream.h"
+#import "OZZipWriteStream+Standard.h"
+#import "OZZipReadStream.h"
+#import "OZZipReadStream+Standard.h"
+#import "OZFileInZipInfo.h"
